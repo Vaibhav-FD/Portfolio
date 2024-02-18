b20f7bddb874418a41e7c7fbd24b962f89e8a2cc
Vaibhav-FD <Vaibhav91201@gmail.com>
2024-02-18 20:48:07 +0530

    Portfolio

---
+++ Portfolio/Script.js
@@ -0,0 +1,42 @@
+let GREETMSG = new Date();
+
+document.getElementById("greet").style.marginBottom = "2.1rem";
+
+let METEXT = document.getElementById("aboutme-text");
+
+
+
+if (GREETMSG.getHours() < 12) {
+  document.getElementById("greet").innerText = "GOOD MORNING ,";
+  document.getElementById("greet").style.color = "#69ccf3";
+}
+
+if (GREETMSG.getHours() > 11) {
+  document.getElementById("greet").innerText = "GOOD AFTERNOON ,";
+  document.getElementById("greet").style.color = "#f99369";
+}
+
+if (GREETMSG.getHours() > 16) {
+  document.getElementById("greet").innerText = "GOOD EVENING ,";
+  document.getElementById("greet").style.color = "#c93f63";
+
+}
+
+
+function EDU() {
+
+  METEXT.innerHTML = `BCA [ Bachelor's Of Computer Application ] 
+  <br> Bhola Bhai Patel Colleage Of Computer Studies , Gandhinagar. 
+  <br>( March 2022 ) 
+  <br><br><hr><br> Front - End Web Development Course <br> TOPS Technologies , Ahmedabad. 
+  <br> ( Pursuing )`;
+
+}
+
+function PERINFO() {
+
+  METEXT.innerHTML = `Age : 22 
+  <br><br> Mobile No : +91 9558172253 
+  <br><br> City : Kalol , Gujarat. `;
+
+}
