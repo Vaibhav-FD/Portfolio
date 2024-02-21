ad2621d457fae991266334acdfe72d820f7dd5f9
Vaibhav-FD <Vaibhav91201@gmail.com>
2024-02-21 10:32:15 +0530

    Portfolio

--- Portfolio/Script.js
+++ Portfolio/Script.js
@@ -1,4 +1,4 @@
-let GREETMSG = new Date();
+let greetingMsg = new Date();
 
 document.getElementById("greet").style.marginBottom = "2.1rem";
 
@@ -6,24 +6,24 @@ let METEXT = document.getElementById("about-text");
 
 
 
-if (GREETMSG.getHours() < 12) {
+if (greetingMsg.getHours() < 12) {
   document.getElementById("greet").innerText = "GOOD MORNING ,";
   document.getElementById("greet").style.color = "#69ccf3";
 }
 
-if (GREETMSG.getHours() > 11) {
+if (greetingMsg.getHours() > 11) {
   document.getElementById("greet").innerText = "GOOD AFTERNOON ,";
   document.getElementById("greet").style.color = "#f99369";
 }
 
-if (GREETMSG.getHours() > 16) {
+if (greetingMsg.getHours() > 16) {
   document.getElementById("greet").innerText = "GOOD EVENING ,";
   document.getElementById("greet").style.color = "#c93f63";
 
 }
 
 
-function EDU() {
+function education() {
 
   METEXT.innerHTML = `BCA [ Bachelor's Of Computer Application ] 
   <br> Bhola Bhai Patel Colleage Of Computer Studies , Gandhinagar. 
@@ -33,7 +33,7 @@ function EDU() {
 
 }
 
-function PERINFO() {
+function personalInfo() {
 
   METEXT.innerHTML = `Age : 22 
   <br><br> Mobile No : +91 9558172253 
